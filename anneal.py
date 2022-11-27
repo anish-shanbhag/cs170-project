c28ea8cf53058f4d8b14b33615f986223968a08c
Anish Shanbhag <anish.shanbhag@yahoo.com>
2022-11-27 13:45:52 -0800

    More outputs

--- anneal.py
+++ anneal.py
@@ -58,7 +58,7 @@ def anneal(name: str):
             log(f"SKIPPING OUTPUT UPDATE for {name}:", score(G))
 
 
-i = 0
+i = 14
 for i in list(range(260))[i * 18 : (i + 1) * 18]:
     anneal("small" + str(i + 1))
     anneal("medium" + str(i + 1))
