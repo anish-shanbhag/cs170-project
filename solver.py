ba9bd7b905e4e521acc03fc3c7391d1bd112087e
Harsha Gundala <harshag@berkeley.edu>
2022-11-27 21:54:26 -0800

    harsha's changes

--- solver.py
+++ solver.py
@@ -1,31 +1,19 @@
 import networkx as nx
 import pulp as pl
+import numpy as np
+import networkx as nx
+from sklearn.cluster import SpectralClustering
+from sklearn import metrics
 
-from starter import read_input
+from starter import *
 
+from scipy import sparse
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+import sys
 
-def solve(G: nx.Graph):
-    # TODO implement this function with your solver
-    # Assign a team to v with G.nodes[v]['team'] = team_id
-    # Access the team of v with team_id = G.nodes[v]['team']
-    size = len(G.nodes)
-    model = pl.LpProblem("CS 170 Solver", pl.LpMinimize)
-    objective = None
-    x = [pl.LpVariable(f"x_{i}", 1, size, pl.LpInteger) for i in range(1, size + 1)]
-    c = []
-    for i in range(len(G.nodes)):
-        for j in range(i + 1, len(G.nodes)):
-            if G.has_edge(i, j):
-                d = pl.LpVariable(f"d_{i}_{j}", None, None, pl.LpInteger)
-                model += d >= x[i] - x[j]
-                model += d >= x[j] - x[i]
-                m = pl.LpVariable(f"m_{i}_{j}", None, None, pl.LpBinary)
-                model += m <= d
-                w = pl.LpVariable(f"w_{i}_{j}", None, None, pl.LpInteger)
-                weight = G.edges[i, j]["weight"]
-                model += w == weight * (1 - m)
-                c += [w]
-
-
-G = read_input("ours/small.in")
-solve(G)
+from sklearn.cluster import KMeans
+from networkx.algorithms.community import kernighan_lin_bisection
